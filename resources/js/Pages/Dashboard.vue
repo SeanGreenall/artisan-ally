cd47abbe273636e16b0a58375dd9a3df1f7e50be
Sean Allen <seanallengreenall@live.com>
2024-05-30 13:50:56 +0100

    Start

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -14,7 +14,11 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="p-6 text-gray-900">Inventory Management</div>
+                    <div class="p-6 text-gray-900">Order Management</div>
+                    <div class="p-6 text-gray-900">Supplier Management</div>
+                    <div class="p-6 text-gray-900">Reporting and Analytics</div>
+                    <div class="p-6 text-gray-900">Alerts and Notifications</div>
                 </div>
             </div>
         </div>
